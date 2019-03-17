89d6a285e1147fe1a925f51088761f887e45d706
Artem <artemtryhub@gmail.com>
2019-03-17 12:18:12 +0200

    Added new routes for employees and departments

--- public_html/configs/routes.php
+++ public_html/configs/routes.php
@@ -1,6 +1,9 @@
 <?php
 
 return array(
+    'employees/page-([0-9+])' => 'employee/index/$1',
     'employees' => 'employee/index',
+    'department/([a-z]+)' => 'departments/index/$1',
+    'departments' => 'department/index',
     '' => 'home/index'
 );
