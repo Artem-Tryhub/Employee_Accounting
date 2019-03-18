2a7b4cd6dee2d0a03fbedd9de1b25317e86a7f8e
Artem <artemtryhub@gmail.com>
2019-03-18 23:20:43 +0200

    added href for posts

--- public_html/views/layout/header.php
+++ public_html/views/layout/header.php
@@ -6,6 +6,6 @@
 <h1 align="center">Home</h1>
 <strong><p align="right"><a href="">Login  </a><a href="">Registration</a></p></strong>
 <h2 align="center"><a href="/departments">Departments</a>
-    <a href="">Posts</a>
+    <a href="/posts">Posts</a>
     <a href="/employees">Employees</a></h2>
 <hr/>
