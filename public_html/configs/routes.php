960b5cf60fbeab562fd564d05860410bc82c249e
Artem <artemtryhub@gmail.com>
2019-03-18 23:10:59 +0200

    Added routes for posts

--- public_html/configs/routes.php
+++ public_html/configs/routes.php
@@ -3,7 +3,9 @@
 return array(
     'employees/page-([0-9+])' => 'employee/index/$1',
     'employees' => 'employee/index',
-    'department/([a-z]+)' => 'departments/index/$1',
+    'department/([A-z]+)' => 'employee/byDepart/$1',
     'departments' => 'department/index',
+    'post/([0-9]+)' => 'employee/byPost/$1',
+    'posts' => 'post/index',
     '' => 'home/index'
 );
