40ae5d8244468d9557077249f3daf93ed3176b66
Artem <artemtryhub@gmail.com>
2019-03-18 23:16:06 +0200

    Added method get post by department and post

--- public_html/models/Employee.php
+++ public_html/models/Employee.php
@@ -37,8 +37,51 @@ class Employee
         return $result['COUNT(id)'];
     }
 
-    public static function getByDepartment($departName)
+    public static function getByDepart($departName)
     {
+        $db = Db::getConnection();
+        $departId = Department::getDepartId($departName);
+        $query = "SELECT * FROM employees WHERE id_departments = :departId";
+        $result = $db->prepare($query);
+        $result->bindParam(':departId', $departId, PDO::PARAM_INT);
+        $result->execute();
+        $employeesList = array();
+        $i = 0;
+        while ($row = $result->fetch()) {
+            $employeesList[$i]['id'] = $row['id'];
+            $employeesList[$i]['name'] = $row['name'];
+            $employeesList[$i]['surname'] = $row['surname'];
+            $employeesList[$i]['patronymic'] = $row['patronymic'];
+            $employeesList[$i]['birthday'] = $row['birthday'];
+            $employeesList[$i]['id_posts'] = $row['id_posts'];
+            $employeesList[$i]['payment_type'] = $row['payment_type'];
+            $employeesList[$i]['salary'] = $row['salary'];
+            $i++;
+        }
+        return $employeesList;
+    }
 
+    public static function getByPost($postId)
+    {
+        $db = Db::getConnection();
+        $query = "SELECT * FROM employees WHERE  id_posts = :postId";
+        $result = $db->prepare($query);
+        $result->bindParam(':postId', $postId, PDO::PARAM_INT);
+        $result->execute();
+        $employeesList = array();
+        $i = 0;
+        while ($row = $result->fetch()) {
+            $employeesList[$i]['id'] = $row['id'];
+            $employeesList[$i]['name'] = $row['name'];
+            $employeesList[$i]['surname'] = $row['surname'];
+            $employeesList[$i]['patronymic'] = $row['patronymic'];
+            $employeesList[$i]['birthday'] = $row['birthday'];
+            $employeesList[$i]['id_departments'] = $row['id_departments'];
+            $employeesList[$i]['payment_type'] = $row['payment_type'];
+            $employeesList[$i]['salary'] = $row['salary'];
+            $i++;
+        }
+
+        return $employeesList;
     }
 }
